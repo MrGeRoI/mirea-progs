ddff2dcee2801a51947ebdf52d2fe4c484bbba62
MrGeRoI <47894184+MrGeRoI@users.noreply.github.com>
2023-03-01 00:11:54 +0300

    update govno

--- semestr2/residue.jl
+++ semestr2/residue.jl
@@ -6,11 +6,14 @@ include("arithmetics.jl")
 
 struct Residue{T,M}
     value::T
+	module_::Any
 
-    Residue{T,M}(a::T) where {T,M} = new(mod(a,M))
+    Residue{T,M}(a::T) where {T,M} = new(mod(a,M),M)
 end
 
 get_value(r::Residue) = r.value
+
+get_module(r::Residue) = r.module_
 
 #=
 
@@ -23,22 +26,24 @@ function Base. +(a::Residue{T,M},b::Residue{T,M})::Residue{T,M} where {T,M}
 	return Residue{T,M}(a.value + b.value)
 end
 
-function Base. -(r::Residue{T,M})::Residue{T,M} where {T,M}
-	return Residue{T,M}( r + M * ( r > 0 ? -1 : 1 ) )
+function Base.:-(r::Residue{T,M})::Residue{T,M} where {T,M}
+	return Residue{T,M}( -r )
 end
 
 function Base. -(a::Residue{T,M},b::Residue{T,M})::Residue{T,M} where {T,M}
-	return Residue{T,M}(a.value + b.value)
+	return Residue{T,M}(a.value - b.value)
 end
 
 function inverse(r::Residue{T,M})::Residue{T,M} where {T,M}
-	return Residue{T,M}(invmod_(r.value,M))
+	return Residue{T,M}(invmod(r.value,M))
 end
 
-# display(r::Residue) = display(r.value) # Говно, которое не делает ничего
+function Base. *(a::Residue{T,M},b::Residue{T,M})::Residue{T,M} where {T,M}
+	return Residue{T,M}(a.value ^ b.value)
+end
 
-println( Residue{Integer,3}(-25) )
+function Base. ^(a::Residue{T,M},b::Residue{T,M})::Residue{T,M} where {T,M}
+	return Residue{T,M}(a.value ^ b.value)
+end
 
-println( Residue{Integer,9}(12) )
-
-println( Residue{Integer,7}(2) + Residue{Integer,7}(3) )
+Base.display(r::Residue) = Base.display(r.value)
--- semestr2/test.jl
+++ semestr2/test.jl
@@ -1,16 +1,104 @@
-include("arithmetics.jl")
+include("polynomial.jl")
 
-print("НОД (5,3) ")
+println("====================")
+
+print("НОД (5,3) = ")
 println(gcd_(5,3))
+
+println("====================")
+
+print("НОД (12,4) = ")
+println(gcd_(12,4))
+
+println("====================")
 
 print("Алгоритм евклида (6,3): ")
 println(gcdx_(6,3))
 
+println("====================")
+
 print("Обратный к 2 по модулю 7: ")
 println(invmod_(2,7))
+
+println("====================")
 
 print("Обратный к 4 по модулю 8: ")
 println(invmod_(4,8))
 
-print("Диофантово 2*n + 3*m = 5: ")
-println(diaphant_solve(2,3,5))
+println("====================")
+
+print("Диофантово 2*n + 3*m = 5:\n")
+result = diaphant_solve(2,3,5)
+print("\tn = ",result[1],"\n\tm = ",result[2],"\n")
+
+println("====================")
+
+print("Остаток -25 (mod 5):\n")
+display(Residue{Integer,5}(-25))
+
+println("====================")
+
+print("Остаток 12 (mod 9):\n")
+display(Residue{Integer,9}(12))
+
+println("====================")
+
+print("-7 (mod 10) = ")
+display( Residue{Integer,10}(-7) )
+
+println("====================")
+
+print("2 + 3 (mod 7) = ")
+display( Residue{Integer,7}(2) + Residue{Integer,7}(3) )
+
+println("====================")
+
+print("6 * 3 (mod 15) = ")
+display( Residue{Integer,15}(6) + Residue{Integer,15}(3) )
+
+println("====================")
+
+print("2 - 4 (mod 5) = ")
+display( Residue{Integer,5}(2) - Residue{Integer,5}(4) )
+
+println("====================")
+
+print("Обратный к 5 (mod 6) = ")
+display( inverse( Residue{Integer,6}(5) ) )
+
+println("====================")
+
+poly = Polynom{Real}([13.0,9.0,2.0])
+qoly = Polynom{Real}([1.0,-9.0])
+
+print("p(t) := ")
+display(poly)
+
+print("q(t) := ")
+display(qoly)
+
+println("====================")
+
+print("-p(t) = ")
+display(-poly)
+
+println("====================")
+
+print("p(t) + q(t) = ")
+display(poly + qoly)
+
+println("====================")
+
+print("p(t) - q(t) = ")
+display(poly - qoly)
+
+println("====================")
+
+print("p(t) * q(t) = ")
+display(poly * qoly)
+
+println("====================")
+
+println("p(t) (mod 5) = ")
+
+display(Residue{Polynom{Real},5}(poly))
