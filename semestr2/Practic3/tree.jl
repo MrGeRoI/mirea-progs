ff688e968621324f54dc9801dca2a230180f9ab4
MrGeRoI <ymalysh2012@mail.ru>
2023-04-24 01:43:00 +0300

    sosi hui

--- semestr2/Practic3/tree.jl
+++ semestr2/Practic3/tree.jl
@@ -1,3 +1,8 @@
+#5. Написать функции, позволяющие взаимное преобразование различных
+# способов представления корневых деревьев.
+# Рассмотреть следующие способы представления корневого дерева
+
+# с помощью связанных структур.
 struct Node
     index::Int
     children::Vector{Union{Node,Nothing}}
@@ -6,6 +11,7 @@ struct Node
 		new(idx,Union{Node,Nothing}[])
 	end
 
+	# с помощью вложенных векторов;
 	function Node(vec::Vector)
 		child = Union{Node,Nothing}[]
 
@@ -16,7 +22,8 @@ struct Node
 		new(vec[end],child)
 	end
 
-	function Node(dict::Dict{Int,Vector{Int}},rootIdx::Int)
+	# с помощью списка смежностей, представленного словарём (Dict{Int, Vector{Union{Int, Nothing}})
+	function Node(dict::Dict{Int,Vector{Union{Int, Nothing}}},rootIdx::Int)
 		nodes = Dict{Int,Node}()
 
 		rootVec = Union{Node,Nothing}[]
@@ -27,11 +34,15 @@ struct Node
 			end
 			
 			for child in vec
-				if child != rootIdx && !haskey(nodes,child)
-					nodes[child] = Node(child)
-				end
+				if isnothing(child)
+					push!((parent == rootIdx) ? rootVec : nodes[parent].children,nothing)
+				else
+					if child != rootIdx && !haskey(nodes,child)
+						nodes[child] = Node(child)
+					end
 
-				push!((parent == rootIdx) ? rootVec : nodes[parent].children,nodes[child])
+					push!((parent == rootIdx) ? rootVec : nodes[parent].children,nodes[child])
+				end
 			end
 		end
 
@@ -74,13 +85,15 @@ function to_vec(root::Node)::Vector
 	return vec
 end
 
-function to_dict!(root::Node,dict::Dict{Int,Vector{Int}})::Nothing
+function to_dict!(root::Node,dict::Dict{Int,Vector{Union{Int, Nothing}}})::Nothing
 	if !haskey(dict,root.index)
 		dict[root.index] = Int[]
 	end
 
 	for node in root.children
-		if !isnothing(node)
+		if isnothing(node)
+			push!(dict[root.index],nothing)
+		else
 			to_dict!(node,dict)
 
 			push!(dict[root.index],node.index)
@@ -88,6 +101,63 @@ function to_dict!(root::Node,dict::Dict{Int,Vector{Int}})::Nothing
 	end
 
 	return nothing
+end
+
+#6. Для дерева, представленного вложенными векторами, реализовать следующие функции:
+
+#6.1. функцию, возвращающую высоту дерева
+function get_height(root::Node)::Integer
+	height = 0
+
+	for node in root.children do
+		height = max(height,get_height(node))
+	end
+
+	return height + 1
+end
+
+#6.2 функцию, возвращающую, число листьв дерева
+function get_leaves(root::Node)::Integer
+	if length(root.children) == 0
+		return 1
+	end
+
+	leaves = 0 
+
+	for node in root.children do
+		leaves += get_leaves(node)
+	end
+
+	return leaves
+end
+
+#6.3 функцию, возвращающую число всех вершин дерева
+function get_nodes(root::Node)::Integer
+	nodes = 1 
+
+	for node in root.children do
+		nodes += get_nodes(node)
+	end
+
+	return nodes
+end
+
+#6.4 функцию, возвращающую наибольшую валентность по выходу вершин дерева
+function get_valence(root::Node)::Integer
+	# что такое валентность?
+end
+
+#6.5 функцию, возвращающую среднюю длину пути к вершинам дерева
+function get_average_path(root::Node)::Integer
+	average = 0
+
+	for node in root.children do
+		average += get_height(node)
+	end
+
+	average /= length(root.children)
+
+	return average
 end
 
 vecTree1 = [
@@ -122,17 +192,19 @@ vecTree1 = [
     1
 ]
 
-dictTree2 = Dict{Int, Vector{Int}}([
+dictTree2 = Dict{Int, Vector{Union{Int, Nothing}}}([
     (1,
         [
             2,
-            3
+            3,
+			nothing,
         ]
     ),
 
     (3,
         [
-            4
+            4,
+			nothing,
         ]
     ),
 
@@ -143,7 +215,7 @@ dictTree2 = Dict{Int, Vector{Int}}([
     ),
 ])
 
-dictTree1 = Dict{Int, Vector{Int}}()
+dictTree1 = Dict{Int, Vector{Union{Int, Nothing}}}()
 to_dict!(Node(vecTree1),dictTree1)
 vecTree2 = to_vec(Node(dictTree2,1))
 
