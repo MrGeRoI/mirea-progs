69603778dcbfb8d11e8044d0288aeaf35303ed7f
MrGeRoI <47894184+MrGeRoI@users.noreply.github.com>
2023-04-26 10:22:53 +0300

    Update part1.jl

--- semestr2/Practic4/part1.jl
+++ semestr2/Practic4/part1.jl
@@ -1,5 +1,7 @@
 using Plots
 
+include("../Practic2/practic.jl")
+
 #1. Написать функцию, вычисляющую n-ю частичную сумму ряда Телора
 # (Маклорена) функции для произвольно заданного значения аргумента x.
 # Сложность алгоритма должна иметь оценку O(n).
@@ -18,10 +20,24 @@ end
 
 println("exp(1) ~= ",exp_taylor(1.0))
 
+
 #2. Написать функцию, вычиляющую значение exp(x) с машинной точностью (с
 # максимально возможной в арифметике с плавающей точкой).
+function exp_machine(x::T)::T where T
+    value = oneunit(T)
+	fact = oneunit(T)
+    
+	while abs(x/fact) > eps(T)
+		value += x / fact
+		x *= x
+		fact *= fact + oneunit(T)
+	end
+
+	return value
+end
+
 function exp_ideal(x::T)::T where T
-	return exp_taylor(Float64(ℯ),Int(trunc(x))) * exp(x - trunc(x))
+	return FastPower(Float64(ℯ),Int(trunc(x))) * exp_machine(x - trunc(x))
 end
 
 println("exp(1) = ",exp_ideal(1.0))
@@ -30,7 +46,7 @@ println("exp(1) = ",exp_ideal(1.0))
 # неотрицательного порядка по ее ряду Тейлора с машинной точностью. Для
 # этого сначала вывести соответствующую рекуррентную формулу,
 # обеспечивающую возможность эффективного вычисления.
-function besselj(M::Integer, x::Real)
+function bessel(M::Integer, x::Real)
     sqrx = x*x
     a = 1/factorial(M)
     m = 1
@@ -48,7 +64,7 @@ end
 #3. Построить семейство графиков этих функций для нескольких порядков, начиная с нулевого порядка.
 values = 0:0.1:20
 myPlot = plot()
-for m in 1:5
-	plot!(myPlot, values, besselj.(m, values))
+for m in 0:5
+	plot!(myPlot, values, bessel.(m, values))
 end
-display(myPlot)
+display(myPlot) #bessel.png
